c620be037bf91026a6bc7029c5449f70c99aa4c3
ricardosilva04 <ricardo.silva04@gmail.com>
2019-04-16 21:47:11 -0300

    Trabalho Final

--- app/src/main/java/br/com/retrofitusuariocrud/activity/MainActivity.java
+++ app/src/main/java/br/com/retrofitusuariocrud/activity/MainActivity.java
@@ -1,9 +1,10 @@
 package br.com.retrofitusuariocrud.activity;
 
-import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -23,6 +24,12 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    static String LOG="LOG: ";
+
+    EditText txtId;
+    EditText txtNome;
+    EditText txtUsername;
+
     UsuarioResource apiUserResource;
     ListView listViewUser;
     List<User> listUser;
@@ -33,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listarUsuarios();
+
+    }
+
+    public void listarUsuarios(){
         apiUserResource = UsuarioApi.getUsuario().create(UsuarioResource.class);
 
         Call<List<User>> get = apiUserResource.get();
@@ -75,7 +87,115 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
     }
 
+    public void addUsuario(View v){
+
+        txtId = findViewById(R.id.txtId);
+        txtNome = findViewById(R.id.txtNome);
+        txtUsername = findViewById(R.id.txtUsername);
+        Integer id = Integer.valueOf(txtId.getText().toString());
+        String nome = txtNome.getText().toString();
+        String username = txtUsername.getText().toString();
+
+
+        final User user = new User().builder().id(id).name(nome).username(username).build();
+
+        apiUserResource = UsuarioApi.getUsuario().create(UsuarioResource.class);
+
+        Call<User> post = apiUserResource.post(user);
+
+        post.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User u = response.body();
+                String dados = String.format("O %s foi inserido com sucesso.", user.getName());
+
+                Toast.makeText(getApplicationContext(),dados, Toast.LENGTH_SHORT).show();
+                Log.i(LOG,u.toString());
+
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e(LOG,t.getMessage());
+            }
+        });
+
+        listarUsuarios();
+
+
+    }
+
+    public void editaUsuario(View v) {
+
+        txtId = findViewById(R.id.txtId);
+        txtNome = findViewById(R.id.txtNome);
+        txtUsername = findViewById(R.id.txtUsername);
+        Integer id = Integer.valueOf(txtId.getText().toString());
+        String nome = txtNome.getText().toString();
+        String username = txtUsername.getText().toString();
+
+        final User user = new User().builder().id(id).name(nome).username(username).build();
+
+        apiUserResource = UsuarioApi.getUsuario().create(UsuarioResource.class);
+
+        Call<User> put = apiUserResource.post(user);
+
+        put.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User u = response.body();
+                String dados = String.format("O %s foi atualizado com sucesso.", user.getName());
+
+                Toast.makeText(getApplicationContext(),dados, Toast.LENGTH_SHORT).show();
+                Log.i(LOG,u.toString());
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e(LOG,t.getMessage());
+            }
+        });
+        listarUsuarios();
+    }
+
+    public void excluirUsuario(View v){
+
+        txtId = findViewById(R.id.txtId);
+        txtNome = findViewById(R.id.txtNome);
+        txtUsername = findViewById(R.id.txtUsername);
+        Integer id = Integer.valueOf(txtId.getText().toString());
+        String nome = txtNome.getText().toString();
+        String username = txtUsername.getText().toString();
+
+        final User user = new User().builder().id(id).name(nome).username(username).build();
+
+        apiUserResource = UsuarioApi.getUsuario().create(UsuarioResource.class);
+
+        Call<User> delete = apiUserResource.post(user);
+
+        delete.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User u = response.body();
+                String dados = String.format("O %s foi deletado com sucesso.", user.getName());
+
+                Toast.makeText(getApplicationContext(),dados, Toast.LENGTH_SHORT).show();
+                Log.i(LOG,u.toString());
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e(LOG,t.getMessage());
+            }
+        });
+        listarUsuarios();
+    }
 
 }
--- app/src/main/java/br/com/retrofitusuariocrud/model/User.java
+++ app/src/main/java/br/com/retrofitusuariocrud/model/User.java
@@ -1,5 +1,18 @@
 package br.com.retrofitusuariocrud.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class User {
 
     private Integer id;
@@ -10,82 +23,4 @@ public class User {
     private String phone;
     private String website;
     private Company company;
-
-    public User() {
-    }
-
-    public User(Integer id, String name, String username, String email, Address address, String phone, String website, Company company) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.address = address;
-        this.phone = phone;
-        this.website = website;
-        this.company = company;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public Company getCompany() {
-        return company;
-    }
-
-    public void setCompany(Company company) {
-        this.company = company;
-    }
 }
